91ae54b9efc12ee6df1fe1d247be7a73ed103355
sabawasabi <ON.0723_i@outlook.jp>
2019-11-25 21:52:31 +0900

    Haml Convert

--- config/application.rb
+++ config/application.rb
@@ -2,15 +2,15 @@ require_relative 'boot'
 
 require 'rails/all'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module ChatSpace
-  config.generators do |g|
-    g.stylesheets false
-    g.javascripts false
-    g.helper false
-    g.test_framework false
+  class Application < Rails::Application
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.test_framework false
+    end
   end
 end
